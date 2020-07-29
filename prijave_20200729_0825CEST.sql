b5e45f13ab7a410f9b819a36a24bc6199a0ced80
Dejan <dejan@dejan-iMac.local>
2020-07-29 08:26:44 +0200

    a

--- prijave_20200729_0825CEST.sql
+++ prijave_20200729_0825CEST.sql
@@ -1,60 +1,36 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
 #
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
+# SQL Export
+# Created by Querious (300050)
+# Created: 29 July 2020 at 08:26:17 CEST
+# Encoding: Unicode (UTF-8)
 #
-# Host: localhost (MySQL 5.7.26)
-# Database: prijave
-# Generation Time: 2020-07-29 06:10:06 +0000
-# ************************************************************
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @ORIG_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET @ORIG_UNIQUE_CHECKS = @@UNIQUE_CHECKS;
+SET UNIQUE_CHECKS = 0;
+
+SET @ORIG_TIME_ZONE = @@TIME_ZONE;
+SET TIME_ZONE = '+00:00';
+
+SET @ORIG_SQL_MODE = @@SQL_MODE;
+SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
 
 
-# Dump of table kategorija
-# ------------------------------------------------------------
 
+DROP TABLE IF EXISTS `prijava`;
 DROP TABLE IF EXISTS `kategorija`;
+
 
 CREATE TABLE `kategorija` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `naziv` varchar(100) NOT NULL,
   `opis` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
-LOCK TABLES `kategorija` WRITE;
-/*!40000 ALTER TABLE `kategorija` DISABLE KEYS */;
-
-INSERT INTO `kategorija` (`id`, `naziv`, `opis`)
-VALUES
-	(1,'Zapošljavanje u toku izborne kampanje','U državnim organima, organima državne uprave, organima lokalne samouprave, organima lokalne uprave, javnim ustanovama i državnim fondovima, u periodu od dana raspisivanja do dana održavanja izbora, mogu se izuzetno zaposliti lica na određeno vrijeme, odnosno zaključiti ugovor za obavljanje privremenih i povremenih poslova, radi obezbjeđivanja neometanog i redovnog odvijanja i funkcionisanja procesa rada tih organa, na osnovu odluke nadležnog organa ovih subjekata, samo ako je to predviđeno aktom o sistematizaciji radnih mjesta. čl.44/1'),
-	(2,'Zloupotreba socijalnih davanja','Budžetskim potrošačkim jedinicama, zabranjena je isplata, odnosno davanje jednokratne novčane pomoći, u skladu sa zakonom kojim se uređuju prava i obavljanje djelatnosti socijalne i dječije zaštite, osim u slučaju smrti člana porodice, teškog oboljenja koje prouzrokuje trajni invaliditet u procentu većem od 50% ili štete na stambenom objektu ili imovini usljed požara ili prirodne nepogode. čl.40/1'),
-	(3,'Otpis dugova za struju, vodu, komunalije','Pravnim licima čiji je osnivač, djelimični ili većinski vlasnik država ili jedinica lokalne samouprave da, u periodu od dana raspisivanja do dana održavanja izbora, kao i mjesec dana nakon izbora, vrše otpis dugova građanima, uključujući račune za utrošenu električnu energiju, vodu, kao i račune za sve vrste javnih usluga. čl.42/1'),
-	(4,'Korišćenje javnih prostorija za aktivnosti u kampanji','Zabranjeno je korišćenje prostorija državnih organa, organa državne uprave, organa lokalne samouprave i organa lokalne uprave, JU, državnih fondova i privrednih društava čiji je osnivač i/ili većinski ili djelimični vlasnik država ili jedinica lokalne samouprave za pripremu i realizaciju aktivnosti kampanje, ukoliko se isti uslovi ne obezbijede svim učesnicima u izbornom procesu. čl.36/1'),
-	(5,'Korišćenje službenih vozila u izbornoj kampanji','Javnim funkcionerima zabranjeno je korišćenje službenih automobila u periodu izborne kampanje, osim u slučajevima službene potrebe čl.43/1'),
-	(6,'Vršenje pritiska na pravna lica, privredna društva i fizička lica','Zabranjeno je političkim subjektima, pravnim i fizičkim licima vršenje pritiska na pravna lica, privredna društva i fizička lica prilikom prikupljanja priloga ili bilo koje druge aktivnosti vezane za izbornu kampanju i finansiranje političkih subjekata. Član 35/1'),
-	(7,'Nedozvoljeno finansiranje kampanje','Zabranjeno je obećavati ili stavljati u izgled političku ili bilo kakvu drugu protivuslugu, privilegiju ili ličnu korist fizičkom ili pravnom licu radi pribavljanja finansijske, materijalne ili nenovčane podrške političkom subjektu.čl.34/1 \r\nZabranjeno je davanje ili primanje priloga u novcu ili obliku proizvoda ili usluga preko trećih lica čl.34/2 \r\nZabranjeno je prikrivanje privatnih izvora finansiranja i iznosa skupljenih iz privatnih izvora finansiranja. čl.34/3 18 \r\nZabranjeno je političkim subjektima, pravnim i fizičkim licima vršenje pritiska na pravna lica, privredna društva i fizička lica prilikom prikupljanja priloga ili bilo koje druge aktivnosti vezane za izbornu kampanju i finansiranje političkih subjekata. čl.35/1\r\n'),
-	(8,'Distribucija propagandnog materijala u državnoj upravi','Zabranjena je distribucija propagandnog materijala i prikupljanje potpisa podrške za predaju izborne liste p.s. i predaju kandidature kandidata za izbor Predsjednika CG u državnim organima, organima državne uprave, organima lokalne samouprave, organima lokalne uprave, JU, državnim fondovima i privrednim društvima čiji je osnivač ili većinski ili djelimični vlasnik država ili jedinica lokalne samouprave. čl.36/2 '),
-	(9,'Korišćenje javne mehanizacije bez naknade','U periodu od šest mjeseci prije roka predviđenog za održavanje izbora, zabranjuje se privrednim društvima čiji je osnivač ili većinski vlasnik država ili jedinica lokalne samouprave, da svoju mehanizaciju i opremu ustupaju trećim licima na upotrebu bez posebne odluke i bez ugovora sa naknadom, izuzev u slučajevima sanacije štete nastale usljed elementamih nepogoda, požara ili sprječavanja širenja epidemije zaraznih bolesti  čl.39/1');
-
-/*!40000 ALTER TABLE `kategorija` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table prijava
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `prijava`;
 
 CREATE TABLE `prijava` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -65,15 +41,29 @@ CREATE TABLE `prijava` (
   `email` varchar(100) DEFAULT NULL,
   `ime` varchar(100) DEFAULT NULL,
   `datum_i` varchar(100) DEFAULT NULL,
-  `status` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+
+
+
+
+LOCK TABLES `kategorija` WRITE;
+INSERT INTO `kategorija` (`id`, `naziv`, `opis`) VALUES 
+	(1,'Zapošljavanje u toku izborne kampanje','U državnim organima, organima državne uprave, organima lokalne samouprave, organima lokalne uprave, javnim ustanovama i državnim fondovima, u periodu od dana raspisivanja do dana održavanja izbora, mogu se izuzetno zaposliti lica na određeno vrijeme, odnosno zaključiti ugovor za obavljanje privremenih i povremenih poslova, radi obezbjeđivanja neometanog i redovnog odvijanja i funkcionisanja procesa rada tih organa, na osnovu odluke nadležnog organa ovih subjekata, samo ako je to predviđeno aktom o sistematizaciji radnih mjesta. čl.44/1'),
+	(2,'Zloupotreba socijalnih davanja','Budžetskim potrošačkim jedinicama, zabranjena je isplata, odnosno davanje jednokratne novčane pomoći, u skladu sa zakonom kojim se uređuju prava i obavljanje djelatnosti socijalne i dječije zaštite, osim u slučaju smrti člana porodice, teškog oboljenja koje prouzrokuje trajni invaliditet u procentu većem od 50% ili štete na stambenom objektu ili imovini usljed požara ili prirodne nepogode. čl.40/1'),
+	(3,'Otpis dugova za struju, vodu, komunalije','Pravnim licima čiji je osnivač, djelimični ili većinski vlasnik država ili jedinica lokalne samouprave da, u periodu od dana raspisivanja do dana održavanja izbora, kao i mjesec dana nakon izbora, vrše otpis dugova građanima, uključujući račune za utrošenu električnu energiju, vodu, kao i račune za sve vrste javnih usluga. čl.42/1'),
+	(4,'Korišćenje javnih prostorija za aktivnosti u kampanji','Zabranjeno je korišćenje prostorija državnih organa, organa državne uprave, organa lokalne samouprave i organa lokalne uprave, JU, državnih fondova i privrednih društava čiji je osnivač i/ili većinski ili djelimični vlasnik država ili jedinica lokalne samouprave za pripremu i realizaciju aktivnosti kampanje, ukoliko se isti uslovi ne obezbijede svim učesnicima u izbornom procesu. čl.36/1'),
+	(5,'Korišćenje službenih vozila u izbornoj kampanji','Javnim funkcionerima zabranjeno je korišćenje službenih automobila u periodu izborne kampanje, osim u slučajevima službene potrebe čl.43/1'),
+	(6,'Vršenje pritiska na pravna lica, privredna društva i fizička lica','Zabranjeno je političkim subjektima, pravnim i fizičkim licima vršenje pritiska na pravna lica, privredna društva i fizička lica prilikom prikupljanja priloga ili bilo koje druge aktivnosti vezane za izbornu kampanju i finansiranje političkih subjekata. Član 35/1'),
+	(7,'Nedozvoljeno finansiranje kampanje','Zabranjeno je obećavati ili stavljati u izgled političku ili bilo kakvu drugu protivuslugu, privilegiju ili ličnu korist fizičkom ili pravnom licu radi pribavljanja finansijske, materijalne ili nenovčane podrške političkom subjektu.čl.34/1 \r\nZabranjeno je davanje ili primanje priloga u novcu ili obliku proizvoda ili usluga preko trećih lica čl.34/2 \r\nZabranjeno je prikrivanje privatnih izvora finansiranja i iznosa skupljenih iz privatnih izvora finansiranja. čl.34/3 18 \r\nZabranjeno je političkim subjektima, pravnim i fizičkim licima vršenje pritiska na pravna lica, privredna društva i fizička lica prilikom prikupljanja priloga ili bilo koje druge aktivnosti vezane za izbornu kampanju i finansiranje političkih subjekata. čl.35/1\r\n'),
+	(8,'Distribucija propagandnog materijala u državnoj upravi','Zabranjena je distribucija propagandnog materijala i prikupljanje potpisa podrške za predaju izborne liste p.s. i predaju kandidature kandidata za izbor Predsjednika CG u državnim organima, organima državne uprave, organima lokalne samouprave, organima lokalne uprave, JU, državnim fondovima i privrednim društvima čiji je osnivač ili većinski ili djelimični vlasnik država ili jedinica lokalne samouprave. čl.36/2 '),
+	(9,'Korišćenje javne mehanizacije bez naknade','U periodu od šest mjeseci prije roka predviđenog za održavanje izbora, zabranjuje se privrednim društvima čiji je osnivač ili većinski vlasnik država ili jedinica lokalne samouprave, da svoju mehanizaciju i opremu ustupaju trećim licima na upotrebu bez posebne odluke i bez ugovora sa naknadom, izuzev u slučajevima sanacije štete nastale usljed elementamih nepogoda, požara ili sprječavanja širenja epidemije zaraznih bolesti  čl.39/1');
+UNLOCK TABLES;
+
 
 LOCK TABLES `prijava` WRITE;
-/*!40000 ALTER TABLE `prijava` DISABLE KEYS */;
-
-INSERT INTO `prijava` (`id`, `kategorija`, `grad`, `primjedba`, `attachment`, `email`, `ime`, `datum_i`, `status`)
-VALUES
+INSERT INTO `prijava` (`id`, `kategorija`, `grad`, `primjedba`, `attachment`, `email`, `ime`, `datum_i`, `status`) VALUES 
 	(1,1,'podgorica','','','dejozcg@gmail.com','Vuko',NULL,1),
 	(2,1,'podgorica','adasd','','dejozcg@gmail.com','Dejan',NULL,1),
 	(3,2,'budva','sadas','','dejozcg@gmail.com','Dejan',NULL,1),
@@ -119,15 +109,23 @@ VALUES
 	(44,1,'podgorica','Snjs','A6EC4767-8B26-4671-8775-13BF50250FBC.jpeg','','','2020-07-28 17:23:13',1),
 	(45,1,'podgorica','Shsh','image.jpg','','','2020-07-28 17:23:13',1),
 	(46,1,'podgorica','das','','','','2020-07-28 17:23:13',1);
-
-/*!40000 ALTER TABLE `prijava` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+SET FOREIGN_KEY_CHECKS = @ORIG_FOREIGN_KEY_CHECKS;
+
+SET UNIQUE_CHECKS = @ORIG_UNIQUE_CHECKS;
+
+SET @ORIG_TIME_ZONE = @@TIME_ZONE;
+SET TIME_ZONE = @ORIG_TIME_ZONE;
+
+SET SQL_MODE = @ORIG_SQL_MODE;
+
+
+
+# Export Finished: 29 July 2020 at 08:26:17 CEST
+
